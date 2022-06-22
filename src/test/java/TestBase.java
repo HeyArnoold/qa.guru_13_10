ff4a5d9ac04e2bc5808e1207e05f4cf44f32f789
Maksim <maks.antonov.2012@gmail.com>
2022-06-22 23:44:11 +0300

    added attachments

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -11,5 +13,13 @@ public class TestBase {
 
         Configuration.baseUrl = "https://www.tinkoff.ru/";
         Configuration.browserSize = "2560x1440";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
